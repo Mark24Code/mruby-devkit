50940e618d975f9eecf8bedef51c2e956be39996
Mark24Code <mark.zhangyoung@gmail.com>
2024-06-06 22:36:24 +0800

    fib sample

---
+++ src/fib.rb
@@ -0,0 +1,17 @@
+def fib(n)
+  return n if n <= 1
+
+  # 创建一个数组来存储斐波那契数
+  fib_sequence = [0, 1] + Array.new(n - 1)
+
+  # 填充数组
+  (2..n).each do |i|
+    fib_sequence[i] = fib_sequence[i - 1] + fib_sequence[i - 2]
+  end
+
+  # 返回所需的斐波那契数
+  fib_sequence[n]
+end
+
+# 测试函数
+puts fib(40) # 应该输出55
