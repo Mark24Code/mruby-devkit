ebbe434b0c1b23e6b8010dcaef97e251e8d755a8
Mark24 <mark.zhangyoung@gmail.com>
2024-06-29 19:32:43 +0800

    refactor: rake task

--- task/agent/wasm.rb
+++ task/agent/wasm.rb
@@ -2,6 +2,6 @@ require_relative "./base"
 
 class WasmAgent < BaseAgent
   def build_code
-    shell "emcc -s WASM=1 -Os -I #{@mruby_include_dir} #{@build_dir}/*.c #{@mruby_include_dir}/libmruby.a -lm -o #{@build_dir}/#{@app_name}.js --closure 1"
+    shell "emcc -s WASM=1 -Os -I #{@mruby_include_dir} #{@build_dir}/*.c #{@mruby_lib_dir}/libmruby.a -lm -o #{@build_dir}/#{@app_name}.js --closure 1"
   end
 end
